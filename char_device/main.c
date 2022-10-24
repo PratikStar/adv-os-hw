7b8a918a58eae04baf6875b66f53904b6b5a2731
Pratik Sutar <sutarpratik2012@gmail.com>
2022-10-24 12:24:59 +0900

    semicolon

--- char_device/main.c
+++ char_device/main.c
@@ -17,8 +17,7 @@
 static int device_open(struct inode *, struct file *);
 static int device_release(struct inode *, struct file *);
 static ssize_t device_read(struct file *, char __user *, size_t, loff_t *);
-static ssize_t device_write(struct file *,
-
+static ssize_t device_write(struct file *, const char __user *, size_t, loff_t *);
 
 #define SUCCESS 0
 #define DEVICE_NAME "chardev" /* Dev name as it appears in /proc/devices */
@@ -26,7 +25,8 @@ static ssize_t device_write(struct file *,
 
 /* Global variables are declared as static, so are global within the file. */
 
-static int major; /* major number assigned to our device driver */
+static int major; 
+/* major number assigned to our device driver */
 
 enum {
     CDEV_NOT_USED = 0,
